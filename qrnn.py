2913b083170e2d4c6aada025dff92e2b026cbeb8
pranavajitnair <pranavajitnair.cse18@itbhu.ac.in>
2020-04-21 00:04:27 +0530

    removed mask

--- qrnn.py
+++ qrnn.py
@@ -14,16 +14,16 @@ class QRNN(nn.Module):
                 self.broadcast=nn.Linear(hidden_size,3*hidden_size)
                 
         def connvolution_step(self,input,hidden=None):
-                x_temp=input[:,:input.shape[1]-1,:]
-                x_temp=torch.cat((torch.zeros(input.shape[0],1,input.shape[2]),x_temp),dim=1)
+                x_temp=input[:input.shape[0]-1,:]
+                x_temp=torch.cat((torch.zeros(1,input.shape[1]),x_temp),dim=0)
                 
-                input=torch.cat((input,x_temp),dim=2)
+                input=torch.cat((input,x_temp),dim=1)
                 output=self.convolution(input)
                 
                 if hidden is not None:
                         output=output+self.broadcast(hidden)
                         
-                cell,f,o=output[:,:,:self.hidden_size],output[:,:,self.hidden_size:2*self.hidden_size],output[:,:,2*self.hidden_size:]
+                cell,f,o=output[:,:self.hidden_size],output[:,self.hidden_size:2*self.hidden_size],output[:,2*self.hidden_size:]
                 
                 return torch.tanh(cell),torch.sigmoid(f),torch.sigmoid(o)
             
@@ -38,34 +38,23 @@ class QRNN(nn.Module):
                         
                 return c_prime,h
         
-        def forward(self,input,mask,c=None,h=None):
+        def forward(self,input,c=None,h=None):
                 cell,f,o=self.connvolution_step(input,h)
                 
-                batch_size=input.shape[0]
-                
-#                output=torch.zeros(batch_size,input.shape[1],self.hidden_size)
-                cell_state=torch.zeros(batch_size,1,self.hidden_size)
-                hidden=torch.zeros(batch_size,1,self.hidden_size)
                 
                 hidden_states=[]
                 cell_states=[]
                 
-                for i in range(input.shape[1]):
-                        O=o[:,i,:].view(batch_size,1,-1)
-                        F=f[:,i,:].view(batch_size,1,-1)
-                        Cell=cell[:,i,:].view(batch_size,1,-1)
+                for i in range(input.shape[0]):
+                        O=o[i,:].view(1,-1)
+                        F=f[i,:].view(1,-1)
+                        Cell=cell[i,:].view(1,-1)
                         
                         c,h=self.recurrent_step(O,F,Cell,c)
                         
                         cell_states.append(c)
                         hidden_states.append(h)
                 
-                hidden_temp=torch.cat(hidden_states,dim=1)
-                cell_temp=torch.cat(cell_states,dim=1)
-                
-                for i in range(batch_size):
-#                        output[i,:mask[i],:]=hidden_temp[i,:mask[i],:]
-                        cell_state[i,0,:]=cell_temp[i,mask[i]-1,:]
-                        hidden[i,0,:]=hidden_temp[i,mask[i]-1,:]
+                hidden_temp=torch.cat(hidden_states,dim=0)
                         
-                return hidden_temp,hidden,cell_state
+                return hidden_temp,h,c
